bd9c70b20846da3d054e807ba1fae98b71b31cd4
Lucas <lucasn4s>
2021-04-28 23:00:05 -0300

    Cria arquivo de constantes para as informações do questionário

---
+++ src/utils/constants/questions.js
@@ -0,0 +1,162 @@
+export default {
+	pq: {
+		title: 'Qualidade Pragmática',
+		fields: [
+			{
+				low: 'Técnico',
+				high: 'Humanizado',
+				value: 'human_technical',
+			},
+			{
+				low: 'Complicado',
+				high: 'Simples',
+				value: 'simple_complicated',
+			},
+			{
+				low: 'Impraticável',
+				high: 'Prático',
+				value: 'practical_impractical',
+			},
+			{
+				low: 'Rebuscado',
+				high: 'Direto',
+				value: 'cumbersome_straightforward',
+			},
+			{
+				low: 'Imprevisível',
+				high: 'Previsível',
+				value: 'predictable_unpredictable',
+			},
+			{
+				low: 'Confuso',
+				high: 'Claramente estruturado',
+				value: 'confusing_clearly-structured',
+			},
+			{
+				low: 'Desorganizado',
+				high: 'Gerenciável',
+				value: 'unruly_manageable',
+			},
+		],
+	},
+	hq_i: {
+		title: 'Qualidade Hedônica - Identidade',
+		fields: [
+			{
+				low: 'Isolador',
+				high: 'Conectivo',
+				value: 'isolating_connective',
+			},
+			{
+				low: 'Amador',
+				high: 'Profissional',
+				value: 'unprofessional_professional',
+			},
+			{
+				low: 'Deselegante',
+				high: 'Elegante',
+				value: 'tacky_stylish',
+			},
+			{
+				low: 'Inferior',
+				high: 'Alto nível',
+				value: 'cheap_premium',
+			},
+			{
+				low: 'Segregador',
+				high: 'Integrador',
+				value: 'alienating_integrating',
+			},
+			{
+				low: 'Afasta-me das pessoas',
+				high: 'Aproxima-me das pessoas',
+				value: 'separates-me_brings-me-closer',
+			},
+			{
+				low: 'Não apresentável',
+				high: 'Apresentável',
+				value: 'unpresentable_presentable',
+			},
+		],
+	},
+	hq_s: {
+		title: 'Qualidade Hedônica - Estímulo',
+		fields: [
+			{
+				low: 'Convencional',
+				high: 'Inventivo',
+				value: 'conventional_inventive',
+			},
+			{
+				low: 'Sem imaginação',
+				high: 'Criativo',
+				value: 'unimaginative_creative',
+			},
+			{
+				low: 'Cauteloso',
+				high: 'Arrojado',
+				value: 'cautious_bold',
+			},
+			{
+				low: 'Conservador',
+				high: 'Inovador',
+				value: 'conservative_innovative',
+			},
+			{
+				low: 'Entediante',
+				high: 'Cativante',
+				value: 'dull_captivating',
+			},
+			{
+				low: 'Pouco exigente',
+				high: 'Desafiador',
+				value: 'undemanding_challenging',
+			},
+			{
+				low: 'Comum',
+				high: 'Original',
+				value: 'ordinary_novel',
+			},
+		],
+	},
+	att: {
+		title: 'Atividade',
+		fields: [
+			{
+				low: 'Desagradável',
+				high: 'Agradável',
+				value: 'unpleasant_pleasant',
+			},
+			{
+				low: 'Feio',
+				high: 'Atraente',
+				value: 'ugly_attractive',
+			},
+			{
+				low: 'Enfadonho',
+				high: 'Simpático',
+				value: 'disagreeable_likeable',
+			},
+			{
+				low: 'Rejeitável',
+				high: 'Convidativo',
+				value: 'rejecting_inviting',
+			},
+			{
+				low: 'Ruim',
+				high: 'Bom',
+				value: 'bad_good',
+			},
+			{
+				low: 'Repulsivo',
+				high: 'Atrativo',
+				value: 'repelling_appealing',
+			},
+			{
+				low: 'Desmotivador',
+				high: 'Motivador',
+				value: 'discouragin_motivating',
+			},
+		],
+	},
+}
