fc2ee887ec84fff55cae78bc2ac81fb58b8e84ff
Lucas <lucasn4s>
2021-04-28 22:58:59 -0300

    Cria componente referente a um bloco de questões

---
+++ src/components/QuestionBlock.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-container>
+		<div
+			v-for="field in question.fields"
+			:key="field.value"
+		>
+			<question :field="field" />
+		</div>
+	</v-container>
+</template>
+
+<script>
+import Question from './Question.vue';
+
+export default {
+	inject: ['$validator'],
+
+	components: {
+		Question,
+	},
+
+	props: {
+		question: {
+			type: Object,
+			default: () => {},
+		},
+	},
+}
+</script>
